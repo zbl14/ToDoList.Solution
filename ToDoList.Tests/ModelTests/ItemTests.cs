c80970a2e5d3c7aa7d56ae1162d257a2af8c2745
zbl14 <ifthereisoneday@gmail.com>
2022-07-12 14:56:25 -0700

    Add a Teardown Method

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -35,6 +41,19 @@ namespace ToDoList.Tests
       string result = newItem.Description;
       //Assert
       Assert.AreEqual(updatedDescription, result);
+    }
+    
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Item> newList = new List<Item> { };
+
+      // Act
+      List<Item> result = Item.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
