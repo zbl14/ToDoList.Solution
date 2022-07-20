4d8b6dac33f92591930c72ab3991adab7c84434f
zbl14 <ifthereisoneday@gmail.com>
2022-07-19 23:02:54 -0700

    create Category object, return the name of the category

---
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class CategoryTests
+  {
+    [TestMethod]
+    public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+    {
+      Category newCategory = new Category("test category");
+      Assert.AreEqual(typeof(Category), newCategory.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      //Act
+      string result = newCategory.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+  }
+}
