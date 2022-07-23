428a2dc4dcb35da000bfbdc6fe3cb96df6810ed1
zbl14 <ifthereisoneday@gmail.com>
2022-07-23 16:04:26 -0700

    update test constructor

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Tests
 {
@@ -11,6 +12,11 @@ namespace ToDoList.Tests
     public void Dispose()
     {
       Item.ClearAll();
+    }
+
+    public ItemTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
     }
 
     [TestMethod]
