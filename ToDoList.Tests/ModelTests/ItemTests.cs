50a3287826fa364c59bb63cf72facef4247d6642
zbl14 <ifthereisoneday@gmail.com>
2022-07-23 18:01:08 -0700

    update Equals(), update GetAll test, test passed

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -62,22 +62,24 @@ namespace ToDoList.Tests
       CollectionAssert.AreEqual(newList, result);
     }
 
-    // [TestMethod]
-    // public void GetAll_ReturnsItems_ItemList()
-    // {
-    //   //Arrange
-    //   string description01 = "Walk the dog";
-    //   string description02 = "Wash the dishes";
-    //   Item newItem1 = new Item(description01);
-    //   Item newItem2 = new Item(description02);
-    //   List<Item> newList = new List<Item> { newItem1, newItem2 };
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      newItem1.Save();
+      Item newItem2 = new Item(description02);
+      newItem2.Save();
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-    //   //Act
-    //   List<Item> result = Item.GetAll();
+      //Act
+      List<Item> result = Item.GetAll();
 
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
 
     [TestMethod]
     public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
@@ -104,7 +106,7 @@ namespace ToDoList.Tests
       //Assert
       CollectionAssert.AreEqual(testList, result);
     }
-    
+
     // [TestMethod]
     // public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
     // {
