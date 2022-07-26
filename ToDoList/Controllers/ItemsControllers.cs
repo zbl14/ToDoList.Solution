3d8cda2be18737457ec4d4d7101f8bbdc8c3f89a
zbl14 <ifthereisoneday@gmail.com>
2022-07-26 16:18:27 -0700

    add Edit() for updating description of an item

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
@@ -37,6 +38,20 @@ namespace ToDoList.Controllers
     {
         Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
         return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+        _db.Entry(item).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
   }
 }
