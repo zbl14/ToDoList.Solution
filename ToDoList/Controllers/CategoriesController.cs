b37768b4df0e1e8c7322e4b0fd8469a35f059bec
zbl14 <ifthereisoneday@gmail.com>
2022-07-27 07:58:36 -0700

    add Index()

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,72 +1,24 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//       List<Category> allCategories = Category.GetAll();
-//       return View(allCategories);
-//     }
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     [HttpGet("/categories/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
-
-//     [HttpPost("/categories")]
-//     public ActionResult Create(string categoryName)
-//     {
-//       Category newCategory = new Category(categoryName);
-//       return RedirectToAction("Index");
-//     }
-  
-//     [HttpGet("/categories/{id}")]
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.Items;
-//       model.Add("category", selectedCategory);
-//       model.Add("items", categoryItems);
-//       return View(model);
-//     }
-
-//     // This one creates new Items within a given Category, not new Categories:
-//     [HttpPost("/categories/{categoryId}/items")]
-//     public ActionResult Create(int categoryId, string itemDescription)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category foundCategory = Category.Find(categoryId);
-//       Item newItem = new Item(itemDescription);
-//       newItem.Save();
-//       foundCategory.AddItem(newItem);
-//       List<Item> categoryItems = foundCategory.Items;
-//       model.Add("items", categoryItems);
-//       model.Add("category", foundCategory);
-//       return View("Show", model);
-//     }
-
-//     [HttpPost("/categories/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//       Category.ClearAll();
-//       return RedirectToAction("Index");
-//     }
-
-//     // [HttpPost("/categories/{categoryId}/items/delete")]
-//     // public ActionResult DeleteAllItems(int categoryId)
-//     // {
-//     //   // Dictionary<string, object> model = new Dictionary<string, object>();
-//     //   Category foundCategory = Category.Find(categoryId);
-//     //   foundCategory.Items.ClearAll();
-//     //   return RedirectToAction("Index");
-//     // }
-//   }
-// }
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
+    
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
+  }
+}
