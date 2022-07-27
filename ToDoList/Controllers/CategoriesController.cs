98efaeed65e68bc07c3987d0690388dd4ca144f4
zbl14 <ifthereisoneday@gmail.com>
2022-07-27 08:02:34 -0700

    add Create(), form for create a new category

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -14,11 +14,24 @@ namespace ToDoList.Controllers
     {
       _db = db;
     }
-    
+
     public ActionResult Index()
     {
       List<Category> model = _db.Categories.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+      _db.Categories.Add(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
