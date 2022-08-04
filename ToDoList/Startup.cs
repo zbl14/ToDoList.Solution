b28fb7a7731e0616680d0bc55c03adf9c71ce6fb
zbl14 <ifthereisoneday@gmail.com>
2022-08-04 11:15:58 -0700

    Overriding Default User Requirements

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -31,7 +31,18 @@ namespace ToDoList
     
       services.AddIdentity<ApplicationUser, IdentityRole>()
         .AddEntityFrameworkStores<ToDoListContext>()
-        .AddDefaultTokenProviders(); 
+        .AddDefaultTokenProviders();
+
+      //Overriding Default User Requirements
+      services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
     }
 
     public void Configure(IApplicationBuilder app)
